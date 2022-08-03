acba53d0088ec1da99f63534b438f77aeece678a
H0tab <ksasha18@mail.ru>
2022-08-03 16:56:18 +0300

    Добавьте файлы проекта.

---
+++ ExchangeRatesTest.API/Program.cs
@@ -0,0 +1,21 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddHttpClient();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+
+app.MapGet("/", () => "Hello!");
+
+app.Run();
