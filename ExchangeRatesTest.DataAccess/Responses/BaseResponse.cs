772855caf0038361a39ba01933a978e829c682ed
H0tab <ksasha18@mail.ru>
2022-08-04 21:14:38 +0300

    base response

---
+++ ExchangeRatesTest.DataAccess/Responses/BaseResponse.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExchangeRatesTest.DataAccess.Responses
+{
+    public class BaseResponse<T> : IBaseResponse<T>
+    {
+        public T Data { get; set; }
+        public string Description { get; set; }
+        public Status Status { get; set; }
+    }
+
+    public interface IBaseResponse<T>
+    {
+        T Data { get; }
+        string Description { get; }
+        Status Status { get; }
+    }
+
+    public enum Status
+    {
+        OK,
+        NotFound,
+        InternalServerError
+    }
+}
