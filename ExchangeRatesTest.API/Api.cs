601728bcc5065f6b6894b98327255e3078ca3ff2
H0tab <ksasha18@mail.ru>
2022-08-04 21:15:40 +0300

    ConfigureAPI extension method

---
+++ ExchangeRatesTest.API/Api.cs
@@ -0,0 +1,40 @@
+﻿using ExchangeRatesTest.BL.Services.Abstract;
+using ExchangeRatesTest.DataAccess.Queries;
+using ExchangeRatesTest.DataAccess.Repositories.Abstract;
+using ExchangeRatesTest.DataAccess.Responses;
+using Microsoft.AspNetCore.Mvc;
+using Results = Microsoft.AspNetCore.Http.Results;
+
+namespace ExchangeRatesTest.API
+{
+    public static class Api
+    {
+        public static void ConfigureApi(this WebApplication app)
+        {
+            app.MapGet("/currencies", GetCurrencies);
+            app.MapGet("/currency/{id}", GetCurrency);
+        }
+
+        private static async Task<IResult> GetCurrencies([FromServices] IValutesService service)
+        {
+            var response = await service.GetAll();
+            if (response.Status == Status.OK)
+            {
+                return Results.Ok(response.Data);
+            }
+
+            return Results.Problem(response.Description);
+        }
+
+        private static async Task<IResult> GetCurrency(string id, [FromServices] IValutesService service)
+        {
+            var response = await service.GetById(id);
+            if (response.Status == Status.OK)
+            {
+                return Results.Ok(response.Data);
+            }
+
+            return Results.Problem(response.Description);
+        }
+    }
+}
--- ExchangeRatesTest.API/Program.cs
+++ ExchangeRatesTest.API/Program.cs
@@ -1,9 +1,26 @@
-var builder = WebApplication.CreateBuilder(args);
+using ExchangeRatesTest.API;
+using ExchangeRatesTest.BL.Helpers;
+using ExchangeRatesTest.BL.Services.Abstract;
+using ExchangeRatesTest.BL.Services.Implementations;
+using ExchangeRatesTest.DataAccess;
+using ExchangeRatesTest.DataAccess.Repositories.Abstract;
+using ExchangeRatesTest.DataAccess.Repositories.Implementations;
+using Microsoft.EntityFrameworkCore;
 
-// Add services to the container.
+var builder = WebApplication.CreateBuilder(args);
+var config = builder.Configuration;
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddHttpClient();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(config.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddScoped<IValutesRepository, EFValutesRepository>();
+builder.Services.AddScoped<IValutesService, ValutesService>();
+builder.Services.AddScoped<ValuteAPIFetcher>();
 
 var app = builder.Build();
 
@@ -15,7 +32,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-
-app.MapGet("/", () => "Hello!");
+app.ConfigureApi();
 
 app.Run();
