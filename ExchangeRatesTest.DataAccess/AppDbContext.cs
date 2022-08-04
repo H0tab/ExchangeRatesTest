3f05b849d58c506b1f27c760c3533b860f687b9b
H0tab <ksasha18@mail.ru>
2022-08-04 18:44:06 +0300

    Data Access Layer

---
+++ ExchangeRatesTest.DataAccess/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExchangeRatesTest.DataAccess.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExchangeRatesTest.DataAccess
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Valute> Valutes { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+    }
+}
