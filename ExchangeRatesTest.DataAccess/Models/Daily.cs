9fe10fd56acb5d869d4e3ba026863554b464727b
H0tab <ksasha18@mail.ru>
2022-08-04 18:43:39 +0300

    Added Daily and Valute models

---
+++ ExchangeRatesTest.DataAccess/Models/Daily.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExchangeRatesTest.DataAccess.Models;
+
+public class Daily
+{
+    public DateTime Date { get; set; }
+    public DateTime PreviousDate { get; set; }
+    public DateTime Timestamp { get; set; }
+    public List<Valute> Valutes { get; set; }
+}
