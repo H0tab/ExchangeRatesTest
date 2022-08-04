7ecde5e7a73f59b34ce78e2ba532e3526e1e47cf
H0tab <ksasha18@mail.ru>
2022-08-04 21:16:21 +0300

    Added valute query params

---
+++ ExchangeRatesTest.DataAccess/Queries/ValuteQueryParameters.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExchangeRatesTest.DataAccess.Queries
+{
+    public class ValuteQueryParameters
+    {
+        private const int maxPageSize = 35;
+        public int PageNumber { get; set; } = 1;
+
+        private int _pageSize = 10;
+        public int PageSize
+        {
+            get
+            {
+                return _pageSize;
+            }
+            set
+            {
+                _pageSize = (value > maxPageSize) ? maxPageSize : value;
+            }
+        }
+
+        public static bool TryParse(string? value, out ValuteQueryParameters? parameters)
+        {
+            var trimmedValue = value?.TrimStart('(').TrimEnd(')');
+            var segments = trimmedValue?.Split(',',
+                StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+            if (segments?.Length == 2
+               && int.TryParse(segments[0], out var pageNumber)
+               && int.TryParse(segments[1], out var pageSize))
+            {
+                parameters = new ValuteQueryParameters {PageNumber = pageNumber, _pageSize = pageSize};
+                return true;
+            }
+
+            parameters = null;
+            return false;
+        }
+    }
+}
