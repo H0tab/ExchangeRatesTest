f80946b2e74688e2e3b330273d82a71ec5cc71f5
H0tab <ksasha18@mail.ru>
2022-08-04 21:15:50 +0300

    migrations

---
+++ ExchangeRatesTest.DataAccess/Migrations/20220804171739_initial.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExchangeRatesTest.DataAccess.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Valutes",
+                columns: table => new
+                {
+                    ID = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    NumCode = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CharCode = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Nominal = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Value = table.Column<float>(type: "real", nullable: false),
+                    Previous = table.Column<float>(type: "real", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Valutes", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Valutes");
+        }
+    }
+}
