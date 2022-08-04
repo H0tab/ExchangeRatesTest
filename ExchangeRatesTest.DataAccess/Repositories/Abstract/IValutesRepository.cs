70006a0e1e92703c457f88c0b5818dee1b7317d5
H0tab <ksasha18@mail.ru>
2022-08-04 18:43:51 +0300

    Added Valutes repository

---
+++ ExchangeRatesTest.DataAccess/Repositories/Abstract/IValutesRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExchangeRatesTest.DataAccess.Models;
+
+namespace ExchangeRatesTest.DataAccess.Repositories.Abstract
+{
+    public interface IValutesRepository
+    {
+        Task<IEnumerable<Valute>> GetAll();
+        Task<Valute> GetById(string id);
+        Task<bool> Create(Valute entity);
+        Task<Valute> Update(Valute entity);
+        Task<bool> Delete(string id);
+    }
+}
---
+++ ExchangeRatesTest.DataAccess/Repositories/Implementations/EFValutesRepository.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExchangeRatesTest.DataAccess.Models;
+using ExchangeRatesTest.DataAccess.Repositories.Abstract;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExchangeRatesTest.DataAccess.Repositories.Implementations
+{
+    public class EFValutesRepository : IValutesRepository
+    {
+        private readonly AppDbContext _dbContext;
+
+        public EFValutesRepository(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+
+        public async Task<IEnumerable<Valute>> GetAll()
+        {
+            return await _dbContext.Valutes.ToListAsync();
+        }
+
+        public async Task<Valute> GetById(string id)
+        {
+            return await _dbContext.Valutes.FindAsync(id);
+        }
+
+        public async Task<bool> Create(Valute entity)
+        {
+            await _dbContext.AddAsync(entity);
+            await _dbContext.SaveChangesAsync();
+
+            return true;
+        }
+
+        public async Task<Valute> Update(Valute entity)
+        {
+            _dbContext.Update(entity);
+            await _dbContext.SaveChangesAsync();
+
+            return entity;
+        }
+
+        public async Task<bool> Delete(string id)
+        {
+            var entityToDelete = await _dbContext.Valutes.FindAsync(id);
+
+            if (entityToDelete == null)
+            {
+                return false;
+            }
+
+            _dbContext.Remove(entityToDelete);
+            await _dbContext.SaveChangesAsync();
+
+            return true;
+        }
+    }
+}
